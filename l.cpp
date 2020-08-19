40466bd3fad008b5711eaad2d2dde829309526db
Ayush-Aditya-444 <62843082+Ayush-Aditya-444@users.noreply.github.com>
2020-08-19 20:10:00 +0530

    divisible by 5 and 11

---
+++ l.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter a number");
+scanf("%d", &n);
+if(n%5==0 && n%11==0){
+	printf("divisible");
+}
+else{
+	printf("no divisible");
+}
+}
+
+	
+	
+
